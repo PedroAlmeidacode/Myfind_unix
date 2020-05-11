0c5f5db5d3c8e7a0c2a52d7ff221da8343a36773
Pedro <plsalmeida18@gmail.com>
2020-05-11 01:37:52 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Myfind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Myfind.dir/main.c.o"
+  "Myfind"
+  "Myfind.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Myfind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,12 @@
+//
+// Created by pedro on 11/05/20.
+//
+
+#ifndef MYFIND_LIBRARY_H
+#define MYFIND_LIBRARY_H
+
+#endif //MYFIND_LIBRARY_H
+
+
+
+
