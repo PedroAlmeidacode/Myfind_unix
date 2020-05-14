309bcf37517596235d4220b040fed71510a1196e
Pedro <plsalmeida18@gmail.com>
2020-05-14 03:35:43 +0100

    Inicial thread structure and util functions

--- library.h
+++ library.h
@@ -19,24 +19,26 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <pthread.h>
 #include <sys/stat.h>
 #include <string.h>
 
-// TODO revr logica implementacao das estruturas
+
+void * search_directory_create_thread_to_search_it(void * param);
+
+
+
 struct threads {
-    struct thread_info *current; // thread que esta a pesquisar de momento
+    struct thread_info *head; // primeiro thread a ser criado
+    struct thread_info *tail; // utima thread criada
+
+    int n_threads; // numero de threads criadas no total
     char *path; // path to search from, in dept
 
-    char *name;
-    char *prefix_name;
-    char *sufix_name;
+    char *name, *prefix_name, *sufix_name;
+    char *iname, *prefix_iname, *sufix_iname;
 
-    char *iname;
-    char *prefix_iname;
-    char *sufix_iname;
-
-    char *type;
+    char type; // X means it was not specified
     bool empty; //starts false
     bool executable; //starts false
     int mmin;
@@ -45,16 +47,67 @@ struct threads {
 };
 
 struct thread_info {
-    struct thread_info *parent; // threads created when found a directory
-    pthread_t id; //id of the thread
-    char **matches; // strings that match the search in this thread
+    pthread_t thread_id;  //id of the thread
 
-    struct thread_info **created; // threads created when found a directory
-    int n_threads_created; //number of threads this thread created
+    char **matches; // strings that match the search in this thread
+    int n_matches; // numnero de matches encontrados ou seja numero de strings em matches
+
+    struct thread_info *next; // threads created when found a directory, nexy = tail->next
 };
 
 
-//function to convert into lowercase
+pthread_mutex_t trinco = PTHREAD_MUTEX_INITIALIZER;
+
+int isSocket(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISSOCK(statbuf.st_mode);
+}
+
+int isCharater(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISCHR(statbuf.st_mode);
+}
+
+int isBlock(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISBLK(statbuf.st_mode);
+}
+
+int isSymbolicLink(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISLNK(statbuf.st_mode);
+}
+
+int isFile(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISREG(statbuf.st_mode);
+}
+
+int isDirectory(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISDIR(statbuf.st_mode);
+}
+
+int isFifo(const char * path){
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return S_ISFIFO(statbuf.st_mode);
+
+}
+
 char *stringLwr(char *s) {
     int len, i;
     len = strlen(s);
@@ -67,20 +120,235 @@ char *stringLwr(char *s) {
 }
 
 
+
+
+/**
+ * Testa se o ficheiro e maior ou menor (especificado em above_size)
+ * size contem o tamanho em Megas
+ * Se size estiver a 0 significa que nao foi especificado nos argumentos
+ * @param size tamanho
+ * @param above_size true para acima de size , false para abaixo de size
+ * @param path ca,imho para o ficheiro a testar
+ * @return true se estiver nos tamanbos estipulados
+ */
+int isAboveorUnderSize(int size, bool above_size, const char *path) {
+    if (size == 0) return 1; // se nao tiver sido explicito nada nos argumentos deste parametro
+    //size expressed in Megabytes
+    long bytes_size = size * 1048576;
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    if(above_size == true ){
+        return bytes_size > statbuf.st_size;
+    }else return bytes_size < statbuf.st_size;
+}
+
+/**
+ * Testa se o ficheiro foi modificado nos ultmos nnmin minutos
+ * Se mmin estiver a 0 significa que nao foi especificado nos argumentos
+ * e sendo assim retorna true
+ * @param nnmin numero de minutos
+ * @param path caminho para o file a testar
+ * @return true se o ficheiro tiver sido modificado dentro do tempo modifcado
+ */
+int isMmin(int nnmin, const char *path) {
+    if (nnmin == 0) return 1;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    struct stat file_stat;
+    int err = stat(path, &file_stat);
+    if (err != 0) {
+        perror(" [file_is_modified] stat");
+        exit(errno);
+    }
+    struct tm *time_of_mod;
+    struct tm *time;
+    stat(path, &file_stat);
+    time_of_mod = gmtime(&(file_stat.st_mtime)); // obtem o tempo da ultima modificacao do ficheiro
+    time = localtime(0); // obtem a hora local
+    // obtem o tempo pedido, o ficheiro nao deve ter sido criado antes deste tempo para retornar verdadeiro
+    time = (struct tm *) (time->tm_min - nnmin);
+    if (time_of_mod > time) return 1; //esta comparacao poder na estar a resultar
+
+    return 0;
+}
+
+/**
+ * Se executable tiver null significa que nao foi especificado nos args
+ * e sendo assim retorna true
+ * @param executable parametro especificado em args
+ * @param path path para o ficheiro a testar
+ * @return true se o ficheiro for executavel
+ */
+int isExecutable(bool executable, const char *path) {
+    if(executable == false) return 1;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    struct stat statbuf;
+    if (stat(path, &statbuf) == 0 && statbuf.st_mode & S_IXUSR)
+        return 1;
+    return 0;
+}
+
+/**
+ * Se empty tiver null significa que nao foi especificado nos args
+ * e sendo assim retorna true
+ * Testa se o ficheiro passado em path esta vazio ou nao
+ * Se estiver vazio retorna true
+ *
+ * @param empty paramtro especificado em args
+ * @param path path para o ficheiro a testar
+ * @return true se o ficheiro tiver vazio
+ */
+int isEmpty(bool empty, const char *path) {
+    if (empty == false) return 1;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0)
+        return 0;
+    return statbuf.st_size == 0; // se o tamanho for 0 retorna 1
+}
+
+/**
+ * Se type tiver x que significa que nao foi especificado nos args
+ * e sendo assim retorna true
+ * De acordo com o caracter em type testa se o ficheiro incluido em path
+ * verifica se
+ *
+ * @param type tipo escolhido em args
+ * @param path path para o ficheiro
+ * @return veradadeiro se for do tipo escolhido
+ */
+int isType(const char type, const char *path) {
+    if (type == 'X') return 1 ;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    switch (type) {
+        case 'c':
+            if (isCharater(path)){
+                return 1;
+            }else return 0;
+        case 'b':
+            if (isBlock(path)){
+                return 1;
+            }else return 0;
+        case 'd':
+            if (isDirectory(path)){
+                return 1;
+            }else return 0;
+        case 'p':
+            if (isFifo(path)){
+                return 1;
+            }else return 0;
+        case 'f':
+            if (isFile(path)){
+                return 1;
+            }else return 0;
+        case 'l':
+            if (isSymbolicLink(path)){
+                return 1;
+            }else return 0;
+        case 's':
+            if (isSocket(path)){
+                return 1;
+            }else return 0;
+        default: return 0;
+    }
+}
+
+/**
+ * esta funcao ignora minusculas e maiusculas
+ * passa d_name para minuscula visto que name ja foi convertido em arg_parse
+ * Se os 3 estiverem a null ele automaticamente retorna true pois nao foi expecificado como argumento
+ * Checka se o nome em d_name e o mesmo que name
+ * ou se tem sub em name
+ * ou se tem pref em name
+ * um dos 3 existe os outros dois estao a null
+ *
+ * @param name parametro
+ * @param suf parametro
+ * @param pref parametro
+ * @param d_name ficheiro em investigacao
+ * @return retorna true se um dos 3 elementos checkar com o d_name
+ */
+int isIname(const char * name, const char * suf, const char * pref, char *d_name) {
+
+    if (name == NULL && suf == NULL && pref == NULL) return 1;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    d_name = stringLwr(d_name); // passa o nome do ficheiro encontrado para minuscula
+    if (name == d_name) return 1;
+    if (pref != NULL){
+        // testar se prefixo == a primeira parte de d_name
+        for (int i = 0; i < strlen(pref) ; i++) {
+            if (d_name[i] != pref[i]){
+                return 0; // se for diferente retorna logo falso
+            }
+        }
+        return 1; // se sair fora do for quer dizer que pref e igual as primeiras strlen(pref) posicoes de pref
+    }
+    // testar se o sufixo  == a ultima parte de d_name
+    int last_pos_suf = strlen(suf) -1;
+    int last_pos_name = strlen(name) - 1;
+    while(last_pos_suf >= 0){
+        if (d_name[last_pos_name] != suf[last_pos_suf]){
+            return 0; // se for diferente retorna logo falso
+        }
+        last_pos_name --;
+        last_pos_suf --;
+    }
+    return 1;
+}
+
+/**
+ * Se os 3 estiverem a null ele automaticamente retorna true pois nao foi expecificado como argumento
+ * Checka se o nome em d_name e o mesmo que name
+ * ou se tem sub em name
+ * ou se tem pref em name
+ * um dos 3 existe os outros dois estao a null
+ *
+ * @param name parametro
+ * @param suf parametro
+ * @param pref parametro
+ * @param d_name ficheiro em investigacao
+ * @return retorna true se um dos 3 elementos checkar com o d_name
+ */
+int isName(const char * name, const char * suf, const char * pref, const char *d_name) {
+    if (name == NULL && suf == NULL && pref == NULL) return 1;  // se nao tiver sido explicito nada nos argumentos deste parametro
+    if (name == d_name) return 1;
+    if (pref != NULL){
+        // testar se prefixo == a primeira parte de d_name
+        for (int i = 0; i < strlen(pref) ; i++) {
+            if (d_name[i] != pref[i]){
+                return 0; // se for diferente retorna logo falso
+            }
+        }
+        return 1; // se sair fora do for quer dizer que pref e igual as primeiras strlen(pref) posicoes de pref
+    }
+    // testar se o sufixo  == a ultima parte de d_name
+    int last_pos_suf = strlen(suf) -1;
+    int last_pos_name = strlen(name) - 1;
+    while(last_pos_suf >= 0){
+        if (d_name[last_pos_name] != suf[last_pos_suf]){
+            return 0; // se for diferente retorna logo falso
+        }
+        last_pos_name --;
+        last_pos_suf --;
+    }
+    return 1;
+}
+
+
+
+
+
+
+
 void print_struct(struct threads args) {
     printf("path: %s\n", args.path);
     printf("name: %s%s%s\n", args.name, args.sufix_name, args.prefix_name);
     printf("iname: %s%s%s\n", args.iname, args.sufix_iname, args.prefix_iname);
-    printf("type: %s\n", args.type);
+    printf("type: %c\n", args.type);
     printf("empty: %d\n", args.empty);
     printf("executable: %d\n", args.executable);
     printf("mmin: %d\n", args.mmin);
     printf("size: %d above this: %d\n", args.size, args.above_size);
 }
 
-
 void inicialize_threads_struct(struct threads *args) {
-    args->current = NULL;
+    args->head = NULL;
+    args->tail = NULL;
     args->path = NULL;
     args->name = NULL;
     args->prefix_name = NULL;
@@ -88,14 +356,13 @@ void inicialize_threads_struct(struct threads *args) {
     args->iname = NULL;
     args->prefix_iname = NULL;
     args->sufix_iname = NULL;
-    args->type = NULL;
+    args->type = 'X';
     args->empty = false;
     args->executable = false;
     args->mmin = 0;
     args->size = 0;
     args->above_size = false;
 }
-
 
 void parse_args(int argc, char *argv[], struct threads *args) {
 
@@ -108,18 +375,20 @@ void parse_args(int argc, char *argv[], struct threads *args) {
         printf("Nao foram especificados argumentos\n");
     }
     if (argc >= 2) {
-
-        if (strcmp(argv[1], ".") == 0) {
-            if (strlen(argv[1]) == 1)args->path = path_wd;
-            else {
+        char tmp = argv[1][0]; // para checkar se a primeria posicao e um ponto
+        if (tmp == '.') {
+            if (strlen(argv[1]) == 1){
+                args->path = malloc(strlen(path_wd) + 1);
+                sprintf(args->path,"%s/",path_wd);
+            }else {
                 char *path_arg = argv[1];
                 memmove(&path_arg[0], &path_arg[1], strlen(path_arg)); //remove a primeira posicao
                 args->path = malloc(strlen(path_wd) + strlen(path_arg));
                 sprintf(args->path, "%s%s", path_wd, path_arg);
             }
-        } else {
-            args->path = malloc(strlen(path_wd) + strlen(argv[1]));
-            sprintf(args->path, "%s%s", path_wd, argv[1]);
+        } else { // se for uma directoria em especifico
+            args->path = malloc(strlen(argv[1]));
+            args->path = argv[1];
         }
         int i = 2;
         while (i < argc) {
@@ -175,8 +444,13 @@ void parse_args(int argc, char *argv[], struct threads *args) {
                 i += 2;
 
             } else if (strcmp(argv[i], "-type") == 0) {
-                args->type = malloc(sizeof(argv[i + 1]));
-                strcpy(args->type, argv[i + 1]);
+                char carecter = (char) argv[1];
+                // se for differente de todas nao esta dentro das escolhas para o tipo
+                if(carecter != 'c' && carecter != 'b' && carecter != 'd' && carecter != 'p' && carecter != 'f' && carecter != 'l' && carecter != 's' && carecter != 'd' ){
+                    printf("type choosed is not acepted\n");
+                    exit(EXIT_FAILURE);
+                }
+                args->type = (char) argv[i + 1];
                 i += 2;
             } else if (strcmp(argv[i], "-empty") == 0) {
                 args->empty = true;
@@ -205,46 +479,15 @@ void parse_args(int argc, char *argv[], struct threads *args) {
 
 }
 
-int isDirectory(const char *path) {
-    struct stat statbuf;
-    if (stat(path, &statbuf) != 0)
-        return 0;
-    return S_ISDIR(statbuf.st_mode);
-}
+void print_matches(struct threads threads){
 
-void search_directory_create_thread_to_search_it(struct threads *args) {
-    // TODO inicializacao da strcut thread_info
-    // TODO colocar current->created = thread_info e atualizar current para este thread
-    DIR *dir;
-    struct dirent *entry;
+    struct thread_info *tmp = threads.head; // tmp fica com a primeria thrad inicializada
+    for (int i = 0; i < threads.n_threads ; i++) {
 
-    if ((dir = opendir(args->path)) == NULL)
-        perror("opendir() error");
-    else {
-        printf("contents of root in %s:\n", args->path);
-        // enqyanto os directorios de dir nao forem null
-        char *local_path = args->path; //path do directorio a ser exploraod pela thread
-        while ((entry = readdir(dir)) != NULL) {
-            char * new_path = malloc(300);
-            if (local_path[strlen(local_path)-1] != '/'){ sprintf(new_path, "%s/%s", local_path,entry->d_name);
-            } else sprintf(new_path, "%s%s", local_path,entry->d_name);
-
-            if (isDirectory(new_path) && strcmp(entry->d_name,"..") != 0 && strcmp(entry->d_name,".") != 0) {
-                args->path = new_path;
-
-                // TODO criacao de thread passsando arg como ultimo parametro e a mesma funcao como teceiro
-
-                printf("Encontrou directorio %s\n", entry->d_name);
-                search_directory_create_thread_to_search_it(args);
-            }
-
-            // TODO if de os parametros dos arguementos
-                // TODO guardar na struct a string
+        for (int j = 0; j < tmp->n_matches ; j++) {
+            printf("%s\n",*(tmp->matches + j));
         }
-        closedir(dir);
+        tmp = tmp->next; // tmp fica com a thread inicializada a seguir a mesma
     }
 }
-
-
-
 
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 
+
 #include "library.h"
 
 
@@ -7,25 +8,110 @@
 int main(int argc, char *argv[]) {
 
     /* requisito A */
-    struct threads *args = malloc(sizeof *args); // inicaliza a estrutura que vai conter a informacao geral para os threads
-    // TODO . -> significa de onde esta , qualquer coisa que nao comece por ponto e um sitio especifico
+    struct threads *threads = malloc(sizeof *threads); // inicaliza a estrutura que vai conter a informacao geral para os threads
 
     // altera a estrtura principal, nao precisa de locks pois ainda nao foram criados threads
-    parse_args(argc,argv,args); // faz o parse dos argumentos recebidos do terminal e coloca os dentro da estrtura
+    parse_args(argc,argv,threads); // faz o parse dos argumentos recebidos do terminal e coloca os dentro da estrtura
 
-    print_struct(*args); // imprime a estrutura principal
+    print_struct(*threads); // imprime a estrutura principal
     /* ************ */
 
     /* requsito B */
-    args->path = "/home/pedro/Desktop/Myfind_unix/"; //just for test
 
-    // TODO criacao main thread que chama a funao abaixo
-    search_directory_create_thread_to_search_it(args);
+    // inicializa thread_info
+    // nao precisa de locks porque a concorrencia ainda nao comecou
+    struct thread_info *threadInfo = malloc(sizeof *threadInfo);
+    threads->head = threadInfo;
+    threads->tail = threadInfo;
+    threads->n_threads++; // atualiza o numero de threads lancadas
+    pthread_create(&threadInfo->thread_id, NULL, &search_directory_create_thread_to_search_it, (void *) threads);
+
+    pthread_join(threadInfo->thread_id, NULL);
 
 
+
+
+    // TODO check wrong type in args
     return 0;
 
 
 }
 
+
+// o que passo por parametro tem de ser estatico
+// se for estatico consigo obter a strcut desta thread
+void * search_directory_create_thread_to_search_it(void * param) {
+
+    // o que se passa por parametro nao precisa de lock
+    // coloca localamente a informacao recebida por parametro
+    struct threads * threads;
+    threads = (struct threads*) param;
+
+    // TODO procurar em head to tail de threads una que tenha este thread_id
+
+
+    DIR *dir;
+    struct dirent *entry;
+
+    if ((dir = opendir(threads->path)) == NULL) {
+        perror("opendir() error");
+        printf("Path : %s\n", threads->path);
+    }else {
+        // enqyanto os directorios de dir nao forem null
+        char *local_path = threads->path; //path do directorio a ser exploraod pela thread
+        while ((entry = readdir(dir)) != NULL) {
+
+            char * new_path = malloc(300);
+            if (local_path[strlen(local_path)-1] != '/'){ sprintf(new_path, "%s/%s", local_path,entry->d_name);
+            } else sprintf(new_path, "%s%s", local_path,entry->d_name);
+
+            if (isDirectory(new_path) && strcmp(entry->d_name,"..") != 0 && strcmp(entry->d_name,".") != 0) {
+
+
+                struct thread_info *threadInfo = malloc(sizeof *threadInfo);
+
+                // fecho o trinco
+                pthread_mutex_lock(&trinco);
+                threads->tail->next = threadInfo;   // coloca a thread criadora a apontar para esta
+                threads->tail = threadInfo;         // atualiza a tail para esta thread
+                threads->n_threads++;               // atualiza o numero de threads lancadas
+                threads->path = new_path;           //atualiza o path para a nova thread pesquisar , no novo directorio
+                // abro o trinco
+                pthread_mutex_unlock(&trinco);
+
+
+                printf("Encontrou directorio %s\n", entry->d_name);
+                // lanco thread para explorar este novo path (diretorio)
+                pthread_create(&threadInfo->thread_id, NULL, &search_directory_create_thread_to_search_it, (void *) threads);
+                pthread_join(threadInfo->thread_id, NULL);
+
+
+            }
+            // nao uso if porque pode haver pesquisa por diretorios
+            // caso nao seja pesquisa por diretorio os diretorios sao descartados a primeira funcao do if
+            // checka se o ficheiro esta dentro dos parametros estipulados em argv
+            if(isType(threads->type, new_path) &&
+                isName(threads->name, threads->sufix_name, threads->prefix_name, entry->d_name) &&
+                isIname(threads->iname, threads->sufix_iname, threads->prefix_iname, entry->d_name) &&
+                isEmpty(threads->empty, new_path) &&
+                isExecutable(threads->executable, new_path) &&
+                isMmin(threads->mmin, new_path) &&
+                isAboveorUnderSize(threads->size, threads->above_size, new_path)){
+
+                // TODO envolver por locks
+                // TODO adicionar match a struct info desta thread
+
+
+                // TODO checkar funcionamento das funcoes em separado vendo se resultam como esperado
+
+
+
+            }
+
+
+
+        }
+        closedir(dir);
+    }
+}
 
